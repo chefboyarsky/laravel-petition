be274b8176d3ae0bc31154f16225100aa0c9a347
Aaron Boyarsky <chefboyarsky@gmail.com>
2016-07-30 00:26:58 -0700

    Add unit test for edit/upcate flow and clean up PetitionControllerTest

--- tests/PetitionControllerTest.php
+++ tests/PetitionControllerTest.php
@@ -4,18 +4,14 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+use App\Petition;
+
 class PetitionControllerTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
 
+    /**
+     * Verify the create/save flow works
+     */
     public function testPetitionCreation()
     {
         $user = factory(App\User::class)->create();
@@ -30,7 +26,8 @@ class PetitionControllerTest extends TestCase
              ->type($givenSummary, 'summary')
              ->type($givenBody, 'body')
              ->press('Save')
-             ->seePageIs('/home');
+             ->seePageIs('/home')
+             ->see($givenTitle);
 
         $this->seeInDatabase('petitions', [
             'title'   => $givenTitle,
@@ -39,16 +36,55 @@ class PetitionControllerTest extends TestCase
         ]);
     }
 
-
-    public function testPetitionIndex()
+    /**
+     * Verify the edit/update flow works
+     */ 
+    public function testPetitionUpdate()
     {
         $user = factory(App\User::class)->create();
+
+        $petition = new Petition;
+        $petition->title = "asdf";
+        $petition->summary = "sdfa";
+        $petition->body = "aaff";
+        $petition->user_id = $user->id;
+        $petition->save();
+
+        $this->actingAs($user)
+             ->visit('/petition/1/edit') //verify correct values for fields on edit page
+             ->see('asdf')
+             ->see('sdfa')
+             ->see('aaff')
+             ->type('zzzz', 'title')     //verify fields can be edited
+             ->type('yyyy', 'summary')
+             ->press('Save')             //verify form saves and redirects
+             ->seePageIs('/home')
+             ->see('zzzz')               //verify the updated title is visible on the list page
+             ->visit('/petition/1/edit') //verify all the updated data is still visible when another edit is made
+             ->see('zzzz')
+             ->see('yyyy')
+             ->see('aaff');
+    }
+
+
+     /**
+     * Verify that the title, button, and data for a petition are present
+     */
+    public function testPetitionIndexAction()
+    {
+        $user = factory(App\User::class)->create();
+
+        $petition = new Petition;
+        $petition->title = "asdf";
+        $petition->summary = "sdfa";
+        $petition->body = "aaff";
+        $petition->user_id = $user->id;
+        $petition->save();
 
         $this->actingAs($user)
              ->visit('/petition')
              ->see('Dashboard')
-             ->see('Add New');
-
-        //Could add a few petitions and verify list looks correct
-    }
+             ->see('Add New')
+             ->see('asdf');
+    }   
 }
