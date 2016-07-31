173f138d980df36dba844e32e77cad0294d0e606
Aaron Boyarsky <chefboyarsky@gmail.com>
2016-07-31 16:59:16 -0700

    PHON-32: View detailed info for a petition

--- resources/views/mediafile/create.blade.php
+++ resources/views/mediafile/create.blade.php
@@ -4,7 +4,9 @@
 <div class="container">
     <div class="row">
         <div class="col-md-9 col-md-offset-1">
+
             <div class="panel panel-default">
+
                 <div class="panel-heading">Upload Files
                     <button onclick="location.href='{{ url('/home') }}'" style="float:right; padding:5px 5px 5px 5px; margin:-5px -10px 5px 5px" type="button" class="btn btn-danger">
                         <span class="glyphicon glyphicon-remove"></span>
@@ -19,24 +21,26 @@
                         <input type="submit">
                     </form>
 
-                            @foreach($mediafiles as $mediafile)
-                    <div class="row" style="margin:5px">
-                                <div class="col-md-3 col-md-offset-1">
-                                    <img src="{{ URL::asset('/') . '/public/mediafiles/' . $mediafile->filename }}" style="max-width:100px; max-height:100px"/>
-                                    {!! Form::open([
-                                                'method' => 'DELETE',
-                                                'route' => ['mediafile.destroy', $mediafile->id],
-                                                'class' => 'btn-group'
-                                            ]) !!}
-                                                <button title="Delete Media" id="delete{{ $mediafile->id }}" type="submit" class="btn btn-danger" style="display:inline">
-                                                    <span class="glyphicon glyphicon-remove" style="margin:-5px"></span>
-                                                </button>
-                                    {!! Form::close() !!}
-                                </div>
-                    </div>
-                            @endforeach
+                    @foreach($mediafiles as $mediafile)
+                        <div class="row" style="margin:5px">
+                            <div class="col-md-3 col-md-offset-1">
+                                <img src="{{ URL::asset('/') . '/public/mediafiles/' . $mediafile->filename }}" style="max-width:100px; max-height:100px"/>
+                                {!! Form::open([
+                                    'method' => 'DELETE',
+                                    'route' => ['mediafile.destroy', $mediafile->id],
+                                    'class' => 'btn-group'
+                                ]) !!}
+                                    <button title="Delete Media" id="delete{{ $mediafile->id }}" type="submit" class="btn btn-danger" style="display:inline">
+                                        <span class="glyphicon glyphicon-remove" style="margin:-5px"></span>
+                                    </button>
+                                {!! Form::close() !!}
+                            </div>
+                        </div>
+                    @endforeach
                 </div>
+
             </div>
+
         </div>
     </div>
 </div>
--- resources/views/petition/partials/list.blade.php
+++ resources/views/petition/partials/list.blade.php
@@ -1,61 +1,84 @@
-                    <ul class="list-group">
-                        @foreach( $petitions as $petition )
-                            <li class="list-group-item">
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col-xs-5">
-                                            <div style="margin-top:5px">{{ $petition->title }}</div>
-                                        </div>
+<div class="panel-group" id="accordion">
+    @foreach( $petitions as $petition )
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse{{ $petition->id }}">{{ $petition->title }}</a>
+                </h4>
+            </div>
+            <div id="collapse{{ $petition->id }}" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <div style="margin-top:5px"><b>{{ $petition->summary }}</b>
+                        @if( !$petition->mediafiles->isEmpty() )
+                            <hr/>
+                            @foreach($petition->mediafiles as $mediafile)
+                                <img src="{{ URL::asset('/') . '/public/mediafiles/' . $mediafile->filename }}" style="max-width:100px; max-height:100px"/>
+                            @endforeach
+                        @endif
+                        <hr/>
+                        {{ $petition->body }}
+                    </div>
+                </div>
+                <div class="panel-footer" style="height:50px">
+                    @if ( $controls )
+                        <div style="float:right">
 
-                                        @if ($controls )
-                                        <div class="col-xs-7" style="padding-left:0">
-                                            {!! Form::open([
-                                                'method' => 'PUT',
-                                                'route' => ['petition.publish', $petition->id],
-                                                'class' => 'btn-group'
-                                            ]) !!}
-                                                <button title="{{ $petition->published ? 'Unpublish Petition' : 'Publish Petition' }}"
-                                                        id="publish{{ $petition->id }}" type="submit" class="btn btn-{{ $petition->published ? 'warning' : 'primary' }}">
-                                                    <span class="glyphicon glyphicon-{{ $petition->published ? 'check' : 'globe' }}" style="margin:-5px"></span>
-                                                </button>
-                                                <input type="hidden" class="btn"><!-- fake sibling to right -->
-                                            {!! Form::close() !!}
-                                            {!! Form::open([
-                                                'method' => 'GET',
-                                                'route' => ['petition.mediafiles', $petition->id],
-                                                'class' => 'btn-group'
-                                            ]) !!}
-                                                <input type="hidden" class="btn"><!-- fake sibling to left -->
-                                                <button title="Upload Media" type="submit" class="btn btn-info">
-                                                    <span class="glyphicon glyphicon-file" style="margin:-5px"></span>
-                                                </button>
-                                                <input type="hidden" class="btn"><!-- fake sibling to right -->
-                                            {!! Form::close() !!}
-                                            {!! Form::open([
-                                                'method' => 'GET',
-                                                'route' => ['petition.edit', $petition->id],
-                                                'class' => 'btn-group'
-                                            ]) !!}
-                                                <input type="hidden" class="btn"><!-- fake sibling to left -->
-                                                <button title="Edit Petition" type="submit" class="btn btn-info">
-                                                    <span class="glyphicon glyphicon-pencil" style="margin:-5px"></span>
-                                                </button>
-                                                <input type="hidden" class="btn"><!-- fake sibling to right -->
-                                            {!! Form::close() !!}
-                                            {!! Form::open([
-                                                'method' => 'DELETE',
-                                                'route' => ['petition.destroy', $petition->id],
-                                                'class' => 'btn-group'
-                                            ]) !!}
-                                                <input type="hidden" class="btn"><!-- fake sibling to left -->
-                                                <button title="Delete Petition" id="delete{{ $petition->id }}" type="submit" class="btn btn-danger">
-                                                    <span class="glyphicon glyphicon-remove" style="margin:-5px"></span>
-                                                </button>
-                                            {!! Form::close() !!}
-                                        </div>
-                                        @endif
-                                    </div>
-                                </div>
-                            </li>
-                        @endforeach
-                    </ul>
+                            {!! Form::open([
+                                'method' => 'PUT',
+                                'route' => ['petition.publish', $petition->id],
+                                'class' => 'btn-group'
+                            ]) !!}
+                                <button title="{{ $petition->published ? 'Unpublish Petition' : 'Publish Petition' }}"
+                                        id="publish{{ $petition->id }}"
+                                        type="submit"
+                                        class="btn btn-{{ $petition->published ? 'warning' : 'primary' }}">
+                                    <span class="glyphicon glyphicon-{{ $petition->published ? 'check' : 'globe' }}" style="margin:-5px"></span>
+                                </button>
+                                <input type="hidden" class="btn"><!-- fake sibling to right -->
+                            {!! Form::close() !!}
+
+                            {!! Form::open([
+                                'method' => 'GET',
+                                'route' => ['petition.mediafiles', $petition->id],
+                                'class' => 'btn-group'
+                            ]) !!}
+                                <input type="hidden" class="btn"><!-- fake sibling to left -->
+                                <button title="Upload Media"
+                                        type="submit"
+                                        class="btn btn-info">
+                                    <span class="glyphicon glyphicon-file" style="margin:-5px"></span>
+                                </button>
+                                <input type="hidden" class="btn"><!-- fake sibling to right -->
+                            {!! Form::close() !!}
+
+                            {!! Form::open([
+                                'method' => 'GET',
+                                'route' => ['petition.edit', $petition->id],
+                                'class' => 'btn-group'
+                            ]) !!}
+                                <input type="hidden" class="btn"><!-- fake sibling to left -->
+                                <button title="Edit Petition"
+                                        type="submit"
+                                        class="btn btn-info">
+                                    <span class="glyphicon glyphicon-pencil" style="margin:-5px"></span>
+                                </button>
+                                <input type="hidden" class="btn"><!-- fake sibling to right -->
+                            {!! Form::close() !!}
+
+                            {!! Form::open([
+                                'method' => 'DELETE',
+                                'route' => ['petition.destroy', $petition->id],
+                                'class' => 'btn-group'
+                            ]) !!}
+                                <input type="hidden" class="btn"><!-- fake sibling to left -->
+                                <button title="Delete Petition" id="delete{{ $petition->id }}" type="submit" class="btn btn-danger">
+                                    <span class="glyphicon glyphicon-remove" style="margin:-5px"></span>
+                                </button>
+                            {!! Form::close() !!}
+                        </div>
+                    @endif
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
